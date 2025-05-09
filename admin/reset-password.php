20086417e3609561354f3925691c06c3646e1d76
Sardar219 <sardarhossaini2022@gmail.com>
2025-05-09 11:47:53 +0430

    add the reset page

---
+++ admin/reset-password.php
@@ -0,0 +1,65 @@
+<?php 
+include __DIR__."/layouts/header.php"; 
+require __DIR__."/../config/config.php";
+?>
+
+<?php 
+if(!empty($_POST)){
+    try{
+        if(empty($_POST['password'])){
+            throw new Exception("password is required");
+        }
+        if(empty($_POST['confirm_password'])){
+            throw new Exception('confirm password is required');
+        }
+    }catch(Exception $e){
+        $error_message=$e->getMessage();
+    }
+}
+?>
+<section class="section">
+    <div class="container container-login">
+        <div class="row">
+            <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                <div class="card card-primary border-box">
+                    <div class="card-header card-header-auth">
+                        <h4 class="text-center">Reset Password</h4>
+                    </div>
+                    
+                    <div class="card-body card-body-auth">
+                        <?php 
+                        if(isset($error_message)){
+                            echo $error_message;
+                        }
+                        if(isset($success_message)){
+                            ?><script>alert("<?= $success_message; ?>")</script><?php 
+                        }
+                    ?>
+                        <form method="POST" action="reset-password.php">
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password"  placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="confirm_password"  placeholder="Confirm Password">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg w_100_p">
+                                    Submit
+                                </button>
+                            </div>
+                            <div class="form-group">
+                                <div>
+                                    <a href="login.php">
+                                        back to login page
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include __DIR__."/layouts/footer.php"; ?>
+    
